dc858a20cde71abfeb789037027935d51db7b7bf
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-12 14:36:38 +0100

    add ex 4 & 5 solutions - arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -26,10 +26,14 @@ console.log(teachers);
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = teachers.shift;
-log
+const firstTeacher = teachers.shift();
+console.log(teachers);
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa")
+console.log();
+
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
